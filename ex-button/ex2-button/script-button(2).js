f5a6c02081368a491756e50b51d7d6c8498bd1b5
simon <dvk170511@gmail.com>
2026-06-21 23:44:34 +0700

    feat:Add JavaScript to your Amazon page.

---
+++ ex-button/ex2-button/script-button(2).js
@@ -0,0 +1,8 @@
+ function addtoCart() {
+    const addedMessage = document.getElementById('added');
+    addedMessage.classList.add('show');
+}
+function buyNow() {
+    const boughtMessage = document.getElementById('bought');
+    boughtMessage.classList.add('show');
+}
