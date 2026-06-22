f15e8d64549a23857c1dd88e911132f710c89f24
simon <dvk170511@gmail.com>
2026-06-23 00:36:10 +0700

    feat: exercise change background color

---
+++ ex-javascript/javascript-ex(4)/javascript-ex(4).js
@@ -0,0 +1,14 @@
+function changeToBlue() {
+    document.body.style.backgroundColor = 'lightblue';
+}
+
+function changeToRed() {
+    document.body.style.backgroundColor = 'lightcoral';
+}
+
+function changeToGreen() {
+    document.body.style.backgroundColor = 'lightgreen';
+}
+function changeToDefault() {
+    document.body.style.backgroundColor = '';
+}
