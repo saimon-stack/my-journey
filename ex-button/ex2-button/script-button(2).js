8c705e54901bacc88e85181fe359558aad2e67be
simon <dvk170511@gmail.com>
2026-06-23 00:33:00 +0700

    refactor(exercises): restructure code and update logic/UI
    - Separate monolithic code into individual HTML, CSS, and JS files.
    - Refine JavaScript logic and fix underlying code structure.
    - Adjust CSS styles for a better user interface.

--- ex-button/ex2-button/script-button(2).js
+++ ex-button/ex2-button/script-button(2).js
@@ -1,8 +1,14 @@
- function addtoCart() {
+function addtoCart() {
     const addedMessage = document.getElementById('added');
     addedMessage.classList.add('show');
+    setTimeout( function() {
+        addedMessage.classList.remove('show');
+    }, 2000);
 }
 function buyNow() {
     const boughtMessage = document.getElementById('bought');
     boughtMessage.classList.add('show');
+    setTimeout( function() {
+        boughtMessage.classList.remove('show');
+    }, 2000);
 }
